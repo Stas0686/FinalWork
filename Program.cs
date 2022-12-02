cc6f433217aa1aee17b6f88db6ea12f60279369f
Stanislav <stas@0686@mail.ru>
2022-12-02 04:04:34 +0300

    add functions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] x)
+{
+    Console.WriteLine("Исходный массив: ");
+    for (int i = 0; i < x.Length; i++)
+    {
+        Console.Write("{0}\t", x[i]);
+    }
+    Console.WriteLine();
+    
+}
+
+void ResultArray(string[] y)
+{
+    Console.WriteLine("Результат: ");
+    string[] newarr = new string[y.Length];
+    for (int i = 0; i < y.Length; i++)
+    {
+        if (y[i].Length <= 3)
+        {
+            newarr[i] = y[i];
+            Console.Write("{0}\t", newarr[i]);
+        }
+    }
+}
